858256b23e9cd710dcae7cbf6e3e9788b874c2d6
buskavidas21-cmyk <buskavidas21@gmail.com>
2026-08-05 23:48:59 +0500

    feat: add DELETE /items/:id route

--- src/routes/items.ts
+++ src/routes/items.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { createItem, getItems, getItemsByLocation, getItemById, updateItem, searchItems, getItemByBarcode, assignBarcode } from '../controllers/itemController';
+import { createItem, getItems, getItemsByLocation, getItemById, updateItem, searchItems, getItemByBarcode, assignBarcode, deleteItem } from '../controllers/itemController';
 import { authenticateToken, requireAdmin } from '../middleware/auth';
 import { rejectAuditsFromInventoryRoutes } from '../middleware/auditRole';
 
@@ -13,6 +13,7 @@ router.get('/by-location/:locationId', getItemsByLocation);
 router.get('/barcode/:barcode', getItemByBarcode);
 router.get('/:id', getItemById);
 router.put('/:id', requireAdmin, updateItem);
+router.delete('/:id', requireAdmin, deleteItem);
 router.post('/:id/barcode', requireAdmin, assignBarcode);
 
 export default router;
